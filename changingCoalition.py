875d70d2faac16a0fe965822c98f3c1fbd42eac5
lavik424 <lavi424@gmail.com>
2018-06-24 00:15:24 +0300

    minor change

--- changingCoalition.py
+++ changingCoalition.py
@@ -8,7 +8,7 @@ def changeAndItsPrice(x_train,y_train):
     # changing factors
     x_train.loc[(x_train['Number_of_valued_Kneset_membersFillByMedian'] > 0.5) &
                 (x_train['Will_vote_only_large_partyFillByMode_No'] > 0.5) &
-                (x_train['Overall_happiness_scoreFillByMean'] < 1.083)] *= 1000
+                (x_train['Overall_happiness_scoreFillByMean'] < 1.083),'Number_of_valued_Kneset_membersFillByMedian'] *= 1000
     # x_train.loc[:,'Garden_sqr_meter_per_person_in_residancy_areaFillByMean'] = 1
     # x_train.loc[:,'Will_vote_only_large_partyFillByMode_No'] = 0
     # x_train.loc[:,'Will_vote_only_large_partyFillByMode_Yes'] = 0
